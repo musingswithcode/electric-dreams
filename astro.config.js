c1d53aa2fcdfb04aaa7360fc2f4ddce82471d884
Ajith Ranka <ajithranka@users.noreply.github.com>
2025-01-22 15:05:41 +0000

    add github action for deployment

--- astro.config.js
+++ astro.config.js
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
   devToolbar: {enabled: false},
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+  site: 'https://musingswithcode.github.io',
+  base: 'electric-dreams'
 })
